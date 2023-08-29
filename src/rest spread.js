5d3b45c5778ed1a178a7b5491754b452fd351882
kimjoo <kimjoo278@gmail.com>
2023-08-29 15:01:32 -0500

    asignación de desestructuración

---
+++ src/rest spread.js
@@ -0,0 +1,11 @@
+// arrays destructuring
+
+let fruits = ['apple', 'banana']
+let [a , b] = fruits;
+console.log(a, b);
+
+// object destructuring
+
+let user = {unserName: 'oscar', age: 34}
+let {unserName, age} = user;
+console.log(unserName, age);
