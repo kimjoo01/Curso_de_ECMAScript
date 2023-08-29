69b3e674875540ea2bd7420e5c6c5069c7f5d9cb
kimjoo <kimjoo278@gmail.com>
2023-08-29 14:35:55 -0500

    strings

---
+++ src/strings.js
@@ -0,0 +1,16 @@
+let hello = 'hello';
+let world = 'world';
+let epic = hello + ' ' + world + '!';
+console.log(epic);
+
+// template literals
+
+let epic2 = `${hello} ${world}!`
+console.log(epic2);
+
+// multi line strings
+
+let lorem = `Esto es una frase epica
+esta es la continuacioin de la frase epica
+`
+console.log(lorem)
