8763505a3b6479cd614683d68761a1ee68777df6
kimjoo <kimjoo278@gmail.com>
2023-08-29 15:33:25 -0500

    spread operator

--- src/rest spread.js
+++ src/rest spread.js
@@ -1,11 +1,29 @@
 // arrays destructuring
 
-let fruits = ['apple', 'banana']
+let fruits = ['apple', 'banana'];
 let [a , b] = fruits;
 console.log(a, b);
 
 // object destructuring
 
-let user = {unserName: 'oscar', age: 34}
+let user = {unserName: 'oscar', age: 34};
 let {unserName, age} = user;
 console.log(unserName, age);
+
+// spread operator
+
+let person  = {name: 'luis', age: 28};
+let country = 'MX';
+
+let data = { id: 1,...person, country}
+console.log(data);
+
+// rest 
+
+function sum (num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum (1, 3, 5, 8);
