0f569158f7901c58f269ad778ff97ec3c3736db6
kimjoo <kimjoo278@gmail.com>
2023-08-29 16:16:35 -0500

    object literals

---
+++ object_literals.js
@@ -0,0 +1,12 @@
+// enached osjects literal
+
+function newUser (user, age, country, uid) {
+    return {
+        user,
+        age,
+        country,
+        uid
+    }
+}
+
+console.log(newUser("elPepe", 35, "MX", 1));
